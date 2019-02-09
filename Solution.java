e28e52cf5ffb6a9870735c497988fb8eaf81f9c1
mahdihasnat <44502454+mahdihasnat@users.noreply.github.com>
2019-02-09 14:19:49 +0600

    ok

--- Solution.java
+++ Solution.java
@@ -1,17 +1,5 @@
-
-import java.util.Scanner;
-
 public class Solution {
-
-    public static void main(String[] args) {
-        int t, a, b;
-        Scanner sc = new Scanner(System.in);
-        t = sc.nextInt();
-        while (t-- > 0) {
-            a = sc.nextInt();
-            b = sc.nextInt();
-            System.out.println(a + b);
-        }
-    }
-}
-
+	public static void main(String[] args) {
+		
+	}
+}asdfghj
